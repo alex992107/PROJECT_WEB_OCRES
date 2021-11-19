5a4a820e7658340d524071aacf7bdc2fda0800ef
Theboldhog <clemdepoissy@gmail.com>
2021-11-20 00:43:24 +0100

    Maj

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom' 
 import './index.css';
 import App from './App';
+import Navbar from './Navbar';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -22,7 +24,7 @@ function formatName(user) {
 
 
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Navbar/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
