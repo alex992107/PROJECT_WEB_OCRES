b65ad1cf0a154dc8597abb59542afbc3643398c9
Theboldhog <clemdepoissy@gmail.com>
2021-11-20 01:09:50 +0100

    maj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,11 @@ import {Navbar} from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>
+          <img src='LogoTest.png' alt='moi'/>
+          Logo
+        </Navbar.Brand>
 
       </Navbar>
       
