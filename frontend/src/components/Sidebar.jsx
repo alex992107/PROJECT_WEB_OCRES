5e17e9713a9ba5c8a800edee4bb4724729b31b54
Theboldhog <clemdepoissy@gmail.com>
2021-11-20 15:24:33 +0100

    SideBar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,14 +5,18 @@ import {SidebarData} from "./SidebarData.jsx";
 function Sidebar(){
     return(
         <div className="Side">
+            <ul>
             {SidebarData.map((val,key) => {
-                return(
-                    <li></li>
-                    );
-                }      
-           
-                )
-            }   
+                return (
+                <li key={key} onClick={() => {window.location.pathname = val.link}}>
+                    {""}
+                    <div>{val.icon}</div>
+                    {""}
+                    <div>{val.title}</div>
+                </li>
+                );
+            })}   
+            </ul>
         </div>
     )
 }
