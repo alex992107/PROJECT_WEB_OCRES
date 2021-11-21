f94ec678cf8fe139cef8148bedaee09ed293990d
alex992107 <alex.larnaudie@gmail.com>
2021-11-21 20:47:48 +0100

    bootstrap

--- frontend/src/pages/accueil.jsx
+++ frontend/src/pages/accueil.jsx
@@ -6,15 +6,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Accueil(){
     return(
-      <div>
+      <><div>
         <div className="bordure">
           1
         </div>;
         <div className="Carre2">
-        2
+          2
         </div>;
-      </div>
+      </div><div class="row">
+          <div class="col-xs-6 col-md-3">
+            <a href="#" class="thumbnail">
+            </a>
+          </div>
+        </div></>
     )
+
 }
 
 export default Accueil
