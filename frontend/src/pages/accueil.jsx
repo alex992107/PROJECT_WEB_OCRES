2657ef8ad94e8fdddff24ffba56c7a4a1882eddd
Theboldhog <clemdepoissy@gmail.com>
2021-11-22 20:19:34 +0100

    pas responsive pour le bloc 2

--- frontend/src/pages/accueil.jsx
+++ frontend/src/pages/accueil.jsx
@@ -6,7 +6,7 @@ function Accueil(){
     return(
       <div className="Mid">
         <div className="Col80">
-        Col 1 80%
+        <h1>Col 1 80%</h1>
         <br/>
         1 header titre h1 et un bloc en dessous
         </div>
