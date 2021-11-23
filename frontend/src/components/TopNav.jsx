0d6f3b396da362b319345bf54f16db23b431ad44
Theboldhog <clemdepoissy@gmail.com>
2021-11-23 14:23:24 +0100

    front is done

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import TrainIcon from '@mui/icons-material/Train';
-import { useMediaQuery } from "@mui/material";
 
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -12,7 +11,6 @@ import {Navbar} from 'react-bootstrap'
 import Button from "@restart/ui/esm/Button";
 
 function TopNav(){
-  const isActive=useMediaQuery("(max-width: 100px");
   return(
     <BrowserRouter>
       <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>  
