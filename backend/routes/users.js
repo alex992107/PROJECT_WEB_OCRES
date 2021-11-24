723079852c4acd609f2e38011500c84eb02429a0
Theboldhog <clemdepoissy@gmail.com>
2021-11-24 12:48:52 +0100

    test back

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+let users =[
+  {user:"Clément",
+  id:"0"},
+  {user:"Thibault",
+  id:"1"},
+  {user:"Alexis",
+  id:"2"},
+  {user:"Alexis",
+  id:"3"},
+];
+
+
+router.get('/',(req,res) =>{
+  res.status(200).json({users});
+})
+
+router.get('/:id',(req,res) =>{
+  const {id} = req.params;
+  const {user} = _.find(users,["id", id]);
+
+  res.status(200).json({
+    message:"User found !",user
+  });
+})
+
 
 module.exports = router;
