92cb68b3afc675ed15ccf8013b39be883fdd6947
Theboldhog <clemdepoissy@gmail.com>
2021-11-24 12:48:44 +0100

    test back

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var testRouter = require("./routes/test");
 
 var app = express();
 
@@ -15,5 +16,7 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/test", testRouter);
+
 
 module.exports = app;
