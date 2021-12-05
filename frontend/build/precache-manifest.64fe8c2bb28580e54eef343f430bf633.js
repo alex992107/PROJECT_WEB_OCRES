82720f56fd669485817ba3cd6e883f21d0b96bde
Theboldhog <clemdepoissy@gmail.com>
2021-12-05 15:27:47 +0100

    test deploy

---
+++ frontend/build/precache-manifest.64fe8c2bb28580e54eef343f430bf633.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6e33bece19b719185053530277f2b8",
+    "url": "/PROJECT_WEB_OCRES/index.html"
+  },
+  {
+    "revision": "635b25d2cc2818b05420",
+    "url": "/PROJECT_WEB_OCRES/static/css/main.ddca1406.chunk.css"
+  },
+  {
+    "revision": "666551eaf0181f9c0a5c",
+    "url": "/PROJECT_WEB_OCRES/static/js/2.9258306e.chunk.js"
+  },
+  {
+    "revision": "635b25d2cc2818b05420",
+    "url": "/PROJECT_WEB_OCRES/static/js/main.cb2fd66a.chunk.js"
+  },
+  {
+    "revision": "2694e60170a9d0407ac8",
+    "url": "/PROJECT_WEB_OCRES/static/js/runtime-main.feedf943.js"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38c4f6b5c22cfe8fcd224fc4b9aab78c.js"
+  "/PROJECT_WEB_OCRES/precache-manifest.64fe8c2bb28580e54eef343f430bf633.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PROJECT_WEB_OCRES/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
